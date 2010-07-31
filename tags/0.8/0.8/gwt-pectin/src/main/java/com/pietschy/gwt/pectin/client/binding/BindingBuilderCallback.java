3a5435e1374b59361ba1fdd9fc09e1e6be5e48c3
andrew.pietsch <andrew.pietsch@f2c9d95c-822d-11de-a005-139651f65d8c>
2010-07-31 01:47:33 +0000

    Removed tags/0.8/0.8

--- tags/0.8/0.8/gwt-pectin/src/main/java/com/pietschy/gwt/pectin/client/binding/BindingBuilderCallback.java
+++
@@ -1,9 +0,0 @@
-package com.pietschy.gwt.pectin.client.binding;
-
-/**
- * This interface allows each builder to notify the binder when a new binding is created.
- */
-public interface BindingBuilderCallback
-{
-   void onBindingCreated(AbstractBinding binding, Object target);
-}
--- tags/0.8/0.8/gwt-pectin/src/main/java/com/pietschy/gwt/pectin/client/channel/Publisher.java
+++
@@ -1,9 +0,0 @@
-package com.pietschy.gwt.pectin.client.channel;
-
-/**
- * Interface for publishing values to a Channel.
- */
-public interface Publisher<T>
-{
-   void publish(T value);
-}
